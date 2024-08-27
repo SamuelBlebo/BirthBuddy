bb1cf25b14165a4efa031c80b94f198acc93ee66
Samuel <samuel.blebo@yahoo.com>
2024-08-27 09:38:38 +0000

    Refactor app with a blank template for improved flexibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./component/*.{js,jsx,ts,tsx}",
+    "./screen/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
